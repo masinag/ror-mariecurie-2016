afa3ce012015687021ac96d43f41282366c103ca
Aaron Ciaghi <aaron.ciaghi@gmail.com>
2016-01-18 16:42:36 +0100

    added ruby basic exercises

---
+++ exercises/01_ruby_basics/README.md
@@ -0,0 +1,7 @@
+Istruzioni
+---
+
+0. Scarica `exercise.rb` e `tests.rb` nella stessa directory
+1. Riempi i metodi in `exercise.rb` come richiesto nel commento
+2. Esegui `ruby tests.rb`
+3. Se tutto è corretto vedrai `All the tests passed! Good job!`
---
+++ exercises/01_ruby_basics/exercise.rb
@@ -0,0 +1,29 @@
+# Return a string of greeting
+def welcome_message
+  "Pippo è pelato"
+end
+
+# Implement the factorial function
+def factorial(n)
+  false
+end
+
+# Find the first factorial number greater than m
+def factorial_bigger_than(m)
+  false
+end
+
+# Find the longest string in a vector
+def find_longest_string(array)
+  false
+end
+
+# Find if an array has nested arrays in it
+def has_nested_array?(array)
+  false
+end
+
+# Count the number of upcased letters in a string
+def count_upcased_letters(string)
+  false
+end
