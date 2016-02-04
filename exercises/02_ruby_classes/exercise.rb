6fb4a651eaf5278162b02f5aca435726f55ed839
Aaron Ciaghi <aaron.ciaghi@gmail.com>
2016-02-04 13:47:07 +0100

    fixed exercises

--- exercises/02_ruby_classes/exercise.rb
+++ exercises/02_ruby_classes/exercise.rb
@@ -1,9 +1,22 @@
+# Author deve avere getters e setters per name e surname
 class Author
 end
 
+=begin
+Product deve avere un metodo :valid? che verifica che:
+* name è una stringa non vuota
+* producer è una stringa non vuota
+* code è una stringa di 6 caratteri
+* price è > 0
+=end
 class Product
 end
 
+=begin
+Person deve avere due metodi:
+* bmi: ritorna il BMI basato sul peso e l'altezza
+* bmi_category: ritorna la categoria di BMI sulla base di BMI_CATEGORIES
+=end
 class Person
   BMI_CATEGORIES = {
     very_severely_underweight: 0.0...15.0,
@@ -13,6 +26,6 @@ class Person
     overweight: 25.0...30.0,
     obese: 30.0...35.0,
     severely_ovbese: 35.0...40.0,
-    very_severely_obese: 40.0...Float::INFINITY
+    very_severely_obese: 40.0...999999.99999
   }
 end
--- exercises/02_ruby_classes/exercise_spec.rb
+++ exercises/02_ruby_classes/exercise_spec.rb
@@ -1,49 +1,100 @@
 require './exercise.rb'
 
-describe Author do
-  it 'has attribute accessors' do
-    author = Author.new
-    author.name = 'William'
-    author.surname = 'Shakespeare'
-    exepect(author.name).to eq 'William'
-    exepect(author.surname).to eq 'Shakespeare'
+RSpec.configure do |config|
+  config.mock_with :rspec
+end
+
+RSpec.shared_examples 'when a product is invalid' do
+  it 'should return false' do
+    # expect(@product).to respond_to(:valid?).with(false)
+    expect(product.valid?).to eq false
   end
+end
+
+describe Author do
+  let(:author) { Author.new }
+
+  it 'should have attribute accessors for name and surname' do
+    expect(author).to respond_to(:name=, :surname=, :name, :surname)
+  end
+
 end
 
 describe Product do
-  it 'recognize valid products' do
-    validprod = Product.new
-    validprod.name = 'Cassetta frutta e verdura di stagione'
-    validprod.producer = 'Azienda Agricola Voltolini Debora'
-    validprod.code = 'xyz010'
-    validprod.price = 10.50
-    expect(validprod.valid?).to be true
+  let(:product) { Product.new }
+
+  before do
+    allow(product).to receive_messages(
+      name: 'Cassetta frutta e verdura di stagione',
+      producer: 'Azienda Agricola Voltolini Debora',
+      code: 'xyz010',
+      price: 10.50,
+    )
   end
 
-  it 'recognize invalid products' do
-    invalidprod = Product.new
-    expect(invalidpord.valid?).to be false
+  context "#valid?" do
+    context "when the product's name is empty" do
+      before do
+        allow(product).to receive(:name).and_return('')
+      end
+      it_behaves_like 'when a product is invalid'
+    end
+    context "when the product's producer is empty" do
+      before do
+        allow(product).to receive(:producer).and_return('')
+      end
+      it_behaves_like 'when a product is invalid'
+    end
+    context "when the product's code is 'abcddajghal13215yq122'" do
+      before do
+        allow(product).to receive(:code).and_return('abcddajghal13215yq122')
+      end
+      it_behaves_like 'when a product is invalid'
+    end
+    context "when the price is -19" do
+      before do
+        allow(product).to receive(:price).and_return(-19)
+      end
+      it_behaves_like 'when a product is invalid'
+    end
+
+    context "when a product is valid" do
+      it 'should return true' do
+        expect(product.valid?).to eq true
+      end
+    end
   end
+
+
+
 end
 
 describe Person do
-  it 'has attribute accessors' do
-    person = Person.new(1.8, 80.0)
-    exepect(person.height).to eq 1.8
-    exepect(person.weight).to eq 80.0
-    person.height = 1.95
-    person.weight = 75.0
-    expect(person.height).to eq 1.95
-    exepect(person.weight).to eq 75.0
+  let(:person) { Person.new(1.8, 80.0) }
+
+  it 'should be created with height and weight parameters' do
+    expect { person }.not_to raise_error
   end
 
-  it 'has a bmi method' do
-    person = Person.new(1.8, 80.0)
-    expect(person.bmi).to eq(80.0 / (1.8 ** 2))
+  context "#bmi" do
+    it 'should return a person\'s BMI (weight / (height**2))' do
+      expect(person).to respond_to(:bmi).and_return(80.0 / (1.8 ** 2))
+    end
   end
 
-  it 'has a bmi_category method' do
-    person = Person.new(1.8, 80.0)
-    expect(person.bmi_category).to eq :healthy_weight
+  context "#bmi_category" do
+    Person::BMI_CATEGORIES.each do |key, bmicat|
+      bmi = rand(bmicat)
+      context "when BMI = #{bmi}" do
+        it "should return #{key}" do
+          allow(person).to receive(:bmi).and_return(bmi)
+          expect(person).to respond_to(:bmi_category).and_return(key)
+        end
+      end
+    end
+    it 'should return healthy_weight when height=1.8 and weight=80.0' do
+      expect(person).to respond_to(:bmi_category).and_return(:healthy_weight)
+    end
   end
+
 end
