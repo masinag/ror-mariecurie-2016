795feca1f9f58155384d531508965b255cb7853d
Aaron Ciaghi <aaron.ciaghi@gmail.com>
2016-02-18 14:26:45 +0100

    added sudo where needed

--- utils/reinstall-rbenv.sh
+++ utils/reinstall-rbenv.sh
@@ -2,7 +2,7 @@
 
 cd $HOME;
 
-rm -rf .rbenv
+sudo rm -rf .rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
